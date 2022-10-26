0c2b69c7798aa9a9ea8e56e8205af99fa0822e08
Jayce <thainguyenhoangphatit@gmail.com>
2022-10-26 22:37:48 +0700

    Fix tiny type check

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & { Component: React.ReactNode | any }) {
   return <Component {...pageProps} />;
 }
